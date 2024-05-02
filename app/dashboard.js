7901153a38f526f450e4312cfdbe803ebae356d9
znhg23 <danh.maikhmt23@hcmut.edu.vn>
2024-05-03 01:01:20 +0700

    calendar dashboard

--- app/dashboard.js
+++ app/dashboard.js
@@ -7,21 +7,36 @@ import {
   Image,
   Dimensions,
   TouchableOpacity,
+  Modal,
  } from "react-native";
 import React, { useState, useRef } from "react";
 import { Stack } from "expo-router";
+import moment from 'moment';
+import { Color, FontFamily, Border, FontSize } from "../constants/GlobalStyles";
 import ScreenHeaderBtn from "../components/common/header/ScreenHeaderBtn";
 import NotificationBtn from "../components/common/header/NotificationBtn";
 import DateSwiper from "../components/common/DateSwiper";
-import DateTimePicker from "@react-native-community/datetimepicker"; 
+import DatePicker from "react-native-modern-datepicker"; 
 
 
 const Dashboard = () => {
+  const [open, setOpen] = useState(false);
   const [date, setDate] = useState(new Date());
-
   const onChange = (e, selectedDate) => {
     setDate(selectedDate);
   };
+
+  function handleOnPress () {
+    setOpen(!open);
+  }
+
+  function handleChange (propDate) {
+    setDate(propDate);
+  }
+
+  let currentDate = moment(date).format('YYYY/MM/DD');
+  let previousDate = new Date(date);
+  previousDate.setDate(date.getDate() - 7);
   return (
     <View
       style={{
@@ -48,18 +63,64 @@ const Dashboard = () => {
           },
         }}
       />
-      <DateTimePicker
+      {/* <DateTimePicker
         value={date}
         mode={"date"}
         is24Hour={true}
         onChange={onChange}
-      />
-      <View style={styles.dateContainer}>
-          <Image source={require("../assets/icons/calendar.png")} style={{width: 30, height: 30, marginRight: 10, marginBottom: 12}}/>
+      /> */}
+      <SafeAreaView style={{flex: 1}}>
+        <View style={styles.dateContainer}>
+          <TouchableOpacity onPress={handleOnPress}>
+            <Image source={require("../assets/icons/calendar.png")} style={{width: 20, height: 20, marginRight: 10, marginBottom: 12}}/>
+          </TouchableOpacity>
+          <Modal 
+            animationType='slide'
+            transparent={true}
+            visible={open}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <DatePicker
+                  mode="calendar"
+                  minimumDate={currentDate}
+                  selected={date}
+                  onDateChange={handleChange}
+                />
+
+                <TouchableOpacity onPress={handleOnPress}>
+                  <Text>Close</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
           <Text style={styles.title}>
-            {date.toLocaleString()}
+            {moment(previousDate).format('DD MMM YYYY')} - {moment(date).format('DD MMM YYYY')}
           </Text>
         </View>
+        <View style={styles.statisticsContainer}>
+          {/* Change the content to an API statistics instead of a fixed image */}
+          <Text style={styles.statTitle}>Avg Work Hour</Text>
+          <View style={styles.statGraph}>
+            <Image source={require("../assets/icons/statistics.png")} style={{width: 225, height: 225}}/>
+            <View style={styles.hourText}>
+              <Text style={styles.statTextHour}>9 Hr</Text>
+              <Text style={styles.statTextHour}>8 Hr</Text>
+              <Text style={styles.statTextHour}>7 Hr</Text>
+              <Text style={styles.statTextHour}>6 Hr</Text>
+              <Text style={styles.statTextHour}>5 Hr</Text>
+            </View>
+          </View>
+          <View style={styles.dateText}>
+              <Text style={styles.statTextHour}>15</Text>
+              <Text style={styles.statTextHour}>16</Text>
+              <Text style={styles.statTextHour}>17</Text>
+              <Text style={styles.statTextHour}>18</Text>
+              <Text style={styles.statTextHour}>19</Text>
+              <Text style={styles.statTextHour}>20</Text>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
@@ -68,26 +129,46 @@ export default Dashboard;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0,
+    flex: 1,
     paddingVertical: 24,
     paddingHorizontal: 24,
-    maxHeight: 200,
-    backgroundColor: '#94A3B8',
-    borderBottomLeftRadius: 24,
-    borderBottomRightRadius: 24,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+    backgroundColor: "#0E305D"
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    width: '90%',
+    padding: 35,
+    alignItems: 'center',
+    elevation: 5,
   },
   header: {
     paddingHorizontal: 16,
   },
   title: {
     flex: 0,
-    fontSize: 24,
-    fontWeight: '700',
+    fontSize: 16,
+    fontWeight: '400',
     color: '#0E305D',
     marginBottom: 12,
   },
+  statTitle: {
+    flex: 0,
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#0E305D',
+  },
   dateContainer: {
-    justifyContent: 'center',
+    marginRight: 100,
+    paddingVertical: 50,
+    justifyContent: 'flex-start',
     flexDirection: 'row', // Arrange components horizontally
     alignItems: 'center', // Align items vertically
   },
@@ -96,4 +177,42 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginLeft: 10, // Add spacing between calendarBtn and dateText
   },
+  statisticsContainer: {
+    flex: 0,
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: '#fff',
+    borderRadius: 30,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    elevation: 5,
+  },
+  hourText: {
+    flex: 0,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  dateText: {
+    flex: 0,
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+  },
+  statGraph: {
+    flex: 0,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  statTextHour: {
+    paddingLeft: 20,
+    fontSize: 14,
+    fontWeight: '200',
+    
+  },
+  statDateHour: {
+    paddingLeft: 20,
+    fontSize: 14,
+    fontWeight: '200',
+  }
 })
---
+++ constants/GlobalStyles.js
@@ -0,0 +1,32 @@
+/* fonts */
+export const FontFamily = {
+    iBMPlexSansSemiBold: "IBMPlexSans-SemiBold",
+    iBMPlexSansLight: "IBMPlexSans-Light",
+    montserratBold: "Montserrat-Bold",
+    montserratMedium: "Montserrat-Medium",
+    poppinsMedium: "Poppins-Medium",
+    iBMPlexSansMedium: "IBMPlexSans-Medium",
+    iBMPlexSansRegular: "IBMPlexSans-Regular",
+  };
+  /* font sizes */
+  export const FontSize = {
+    size_17xl: 36,
+    size_smi: 13,
+    size_xs: 12,
+    size_2xs: 11,
+  };
+  /* Colors */
+  export const Color = {
+    colorDarkslateblue: "#0e305d",
+    colorAliceblue: "#eaf2ff",
+    colorGray_100: "#1e1e1e",
+    colorGray_200: "rgba(255, 255, 255, 0)",
+    colorWhite: "#fff",
+  };
+  /* border radiuses */
+  export const Border = {
+    br_8xl: 27,
+    br_6xl: 25,
+    br_11xs: 2,
+  };
+  
